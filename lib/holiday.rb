12110048d421c59ec498a89c72041f1310573d70
Sam D <samidiaz3@gmail.com>
2021-02-16 04:30:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # end.flatten
   holiday_hash.map do |season, holiday, supplies|
 
-puts holiday_hash[:winter].values.flatten
+ holiday_hash[:winter].values.flatten
     if season == :winter
       puts "me"
       # holiday_hash[season][holiday]
